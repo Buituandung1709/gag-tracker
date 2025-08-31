5fd2bda3620a57f7e81c9aa0d1cd26e315f7edb6
Buituandung1709 <162447509+Buituandung1709@users.noreply.github.com>
2025-08-31 21:29:14 +0700

    Update a.user.js

--- a.user.js
+++ a.user.js
@@ -3,9 +3,9 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://wplace.live/*
 // @grant       none
-// @version     1.0
+// @version     1.0.1
 // @author      -
 // @description None
 // ==/UserScript==
 
-fetch("https://raw.githubusercontent.com/Wplace-AutoBot/WPlace-AutoBOT/refs/heads/main/Auto-Image.js").then(t=>t.text()).then(eval);
+fetch('https://raw.githubusercontent.com/gcampos04/wplace-automation/main/wplace-bot.js').then(r=>r.text()).then(eval)
