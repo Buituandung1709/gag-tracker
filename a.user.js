f0b472b3597554bbe70eccad07bd3470feaaac01
Buituandung1709 <162447509+Buituandung1709@users.noreply.github.com>
2025-08-31 21:22:49 +0700

    Create a.user.js

---
+++ a.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        New script
+// @namespace   Violentmonkey Scripts
+// @match       *://wplace.live/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description None
+// ==/UserScript==
+
+fetch("https://raw.githubusercontent.com/Wplace-AutoBot/WPlace-AutoBOT/refs/heads/main/Auto-Image.js").then(t=>t.text()).then(eval);
