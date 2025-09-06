6e282a973ca566f3153784cb6a8f31c300db8d45
Buituandung1709 <162447509+Buituandung1709@users.noreply.github.com>
2025-09-06 19:56:34 +0700

    Create b.user.js

---
+++ b.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Connect To xGreen
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Kết nối tới bot xGreen
+// @match        *://*/*
+// @require      https://pastefy.app/4fQO2qbu/raw
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @connect      *
+// @noframes
+// @icon         https://avatars.githubusercontent.com/u/85283786?s=48&v=4
+// ==/UserScript==
